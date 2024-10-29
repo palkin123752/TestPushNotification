2789019dd8fa3992436da45dc30a6cb6375b8f17
palkin123752 <143208529+palkin123752@users.noreply.github.com>
2024-10-29 16:35:14 +0530

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,29 @@
+importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js');
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAozEohladu0QCS0zgqYBvdQGQBGj-_KNI",
+            authDomain: "mywebapp-89c34.firebaseapp.com",
+            projectId: "mywebapp-89c34",
+            storageBucket: "mywebapp-89c34.appspot.com",
+            messagingSenderId: "406227830633",
+            appId: "1:406227830633:web:3adb4adf63eba35e39dff8",
+            measurementId: "G-HF62RRC88Q"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log("Received background message ", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png' // Optional: Customize the icon path
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
